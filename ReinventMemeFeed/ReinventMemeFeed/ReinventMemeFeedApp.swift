3291f4f0a1bae13f13430f3c6b7963f484811aab
Nicki Stone <nickik@amazon.com>
2021-11-08 11:49:52 -0800

    first commit

---
+++ ReinventMemeFeed/ReinventMemeFeed/ReinventMemeFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReinventMemeFeedApp.swift
+//  ReinventMemeFeed
+//
+//  Created by Stone, Nicki on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ReinventMemeFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
